db4fcf535eeb60d3c22553aa8fcb96fe2f7a11a9
HDDANTE <wk.dante.panella@teamcore.cl>
2026-07-14 20:19:15 -0300

    magia

--- hola.js
+++ hola.js
@@ -1,1 +1,12 @@
 console.log("hola");
+
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  } 
+
+    saludar() {
+    console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    }
+}
